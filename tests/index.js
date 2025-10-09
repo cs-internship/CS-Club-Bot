96be7692a386734ba673534685a034704a2b7c93
Ali Sadeghi <ali909392@gmail.com>
2025-10-09 14:48:59 +0330

    Remove tests and learnSearch, add linting configs
    Deleted all test files and the learnSearch feature. Added ESLint and Prettier configuration files for code style enforcement. Updated dependencies and project metadata in package.json and package-lock.json to support new tooling and remove unused code.

--- tests/index.js
+++
@@ -1,8 +0,0 @@
-// Entry point for all tests. Run with: npm test or npx jest
-// This file will import and run all test suites.
-
-require("./unit/utils.test");
-require("./unit/services.test");
-require("./unit/handlers.test");
-require("./unit/config.test");
-require("./unit/constants.test");
